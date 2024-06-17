d7d33b71cb751fcf32dc4986de5bd084677e5485
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-17 19:28:51 +0530

    disease created

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+const { patientsmodel } = require("./models/patient")
+const jwt = require("jsonwebtoken")
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://anjali2003:anjali2003@cluster0.wy6js.mongodb.net/diseasedb?retryWrites=true&w=majority&appName=Cluster0")
+
+const generateHashedPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(password, salt)
+}
+app.post("/signUp", async (req, res) => {
+
+    let input = req.body
+    let hashedpassword = await generateHashedPassword(input.password)
+    console.log(hashedpassword)
+    input.password = hashedpassword
+    let patient = new patientsmodel(input)
+    patient.save()
+    res.json({ "status": "success" })
+})
