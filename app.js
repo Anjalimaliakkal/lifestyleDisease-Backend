f0d5999374fe40a096d67ff1686bdcfe24ec1d7a
Anjalimaliakkal <147378299+Anjalimaliakkal@users.noreply.github.com>
2024-06-17 21:27:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,33 +25,6 @@ app.post("/signUp", async (req, res) => {
     patient.save()
     res.json({ "status": "success" })
 })
-app.post("/signIn", (req, res) => {
-    let input = req.body
-    patientsmodel.find({ "emailid": req.body.emailid }).then(
-        (response) => {
-            if (response.length > 0) {
-                let dbPassword = response[0].password
-                console.log(dbPassword)
-                bcrypt.compare(input.password, dbPassword, (error, isMatch) => {
-                    if (isMatch) {
-                        jwt.sign({ email: input.emailid }, "patient-app", { expiresIn: "1d" }, (error, token) => {
-                            if (error) {
-                                res.json({ "status": "unable to create token" })
-                            } else {
-                                res.json({ "status": "success", "userid": response[0]._id, "token": token })
-                            }
-                        })
-
-                    } else {
-                        res.json({ "status": "incorrect password" })
-                    }
-                })
-            } else {
-                res.json({ "status": "user doesn't exist" })
-            }
-        }
-    ).catch()
-})
 
 app.post("/signIn", (req, res) => {
     let input = req.body
