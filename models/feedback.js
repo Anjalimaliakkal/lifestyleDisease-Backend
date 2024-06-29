ce34af0dec28fb3a5df812c83d3892a48c237883
Anjalimaliakkal <147378299+Anjalimaliakkal@users.noreply.github.com>
2024-06-29 18:41:21 +0530

    Update feedback.js

--- models/feedback.js
+++ models/feedback.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const schema = mongoose.Schema(
     {
         "rating": { type: String, required: true },
-        "feedback": { type: String, required: true }
+        "comments": { type: String, required: true }
     }
 )
 let feedbacksmodel = mongoose.model("feedback", schema);
